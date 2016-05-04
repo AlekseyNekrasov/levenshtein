6c4fe70dee1ea4c44aa417b6750852f6c1044649
Alexey Nekrasov <Anekrasov@suitsupply.com>
2016-05-04 17:39:23 +0200

    init repo

---
+++ libs/levenshtein.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const matrix = [];
+
+const getDistance = (a, b) => {
+    if (!a.length) return b.length;
+    if (!b.length) return a.length;
+
+    // increment along the first column of each row
+    let i;
+    for (i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+
+    // increment each column in the first row
+    let j;
+    for (j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    // Fill in the rest of the matrix
+    for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+            matrix[i][j] = b.charAt(i - 1) == a.charAt(j - 1) // chars are equal
+                ? matrix[i - 1][j - 1] // prev element on diagonal
+                : _findMinValueAround(matrix, i, j) + 1;
+        }
+    }
+
+    return matrix[b.length][a.length]; // take the last element
+}
+
+const _findMinValueAround = (matrix, i, j) => Math.min(matrix[i - 1][j - 1], Math.min(matrix[i][j - 1], matrix[i - 1][j]));
+
+const buildVisualMatrix = () => {
+  matrix.forEach((elements)=>{
+    console.log(elements);
+    elements.forEach((el)=>{
+      console.log(el)
+    })
+  });
+};
+
+const getMatrix = () => matrix;
+
+module.exports = { getDistance, getMatrix, buildVisualMatrix };
---
+++ tests/checkDistance.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { getDistance, buildVisualMatrix } = require('../libs/levenshtein');
+const expect = require('chai').expect;
+
+describe('Test distance suggestion', () => {
+    it('Should give corrected suggestion.', () => {
+        expect(getDistance('some1', 'some2')).to.equal(1);
+        expect(getDistance('Hello there!', 'Hoi here?')).to.equal(6);
+        expect(getDistance('Same text', 'Same text')).to.equal(0);
+    });
+
+    it('Distance should equal the length of word, if compare with empty string.', () => {
+        const mockText = 'Testing';
+        expect(getDistance(mockText, '')).to.equal(mockText.length);
+    });
+});
