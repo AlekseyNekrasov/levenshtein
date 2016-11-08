f7076a545e6dfe4b53a310c2b94b6ad02022203e
Alexey Nekrasov <Anekrasov@suitsupply.com>
2016-11-08 15:45:08 +0100

    add benchmarks

--- tests/benchmark.js
+++ tests/benchmark.js
@@ -3,8 +3,14 @@ const suite = new Benchmark.Suite;
 const lev = require('../up-levenshtein')
 
 // add tests
-suite.add('getDistance', function(){
+suite.add('Small difference', function(){
   !!lev.getDistance('some1', 'some2')
+})
+.add('Average difference', function(){
+  !!lev.getDistance('alexey@nekrasov.nl', 'alexey@gmail.com')
+})
+.add('Large difference', function(){
+  !!lev.getDistance('I don\'t realy neeed to compare long texts', 'But what wouldn\'t you do for good benchmarks?')
 })
 // add listeners
 .on('cycle', function(event) {
